c4bba07404e050492c539322b91af2b9ce2c204a
noah1400 <48360809+noah1400@users.noreply.github.com>
2023-08-12 19:35:19 +0200

    Update c16vm.h

--- include/c16vm.h
+++ include/c16vm.h
@@ -11,6 +11,14 @@
 #include <c16membank.h>
 #include <c16instructions.h>
 
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
 
 typedef struct _c16vm_t {
     uint16_t banksize;
